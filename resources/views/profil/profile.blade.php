e9f07b29167223ab379a3e495dbabb88537fe913
Helasdic <95930420+Helasdic@users.noreply.github.com>
2024-06-07 15:41:17 +0700

    profile update

--- resources/views/profil/profile.blade.php
+++ resources/views/profil/profile.blade.php
@@ -1,18 +1,85 @@
 @extends('layouts.master')
 
+@section('header')
+    <!-- App Header -->
+    <div class="appHeader bg-primary text-light">
+        <div class="pageTitle">Profile</div>
+    </div>
+    <!-- * App Header -->
+@endsection
+
+
 @section('content')
-<div class="container">
-    <div class="profile-page">
-        <h1>Profile</h1>
-        <div class="profile-info">
-            <img src="" alt="Profile Picture">
-            <h2>name</h2>
-            <p>jabatan</p>
-        </div>
-        <div class="profile-actions">
-            <a href="{{ route('profile.edit') }}">Edit Profile</a>
+
+<div class="section" style="margin-top: 70px;" id="user-section">
+    <div class="container">
+        <div class="profile-page">
+            <div class="profile-info d-flex">
+                <img src="{{asset('assets/img/sample/avatar/avatar1.jpg')}}" alt="Profile Picture" class="rounded-circle" width="70">
+                <div id="user-info">
+                    <h2 id="user-name">Dzikril Hakim</h2>
+                    <span id="user-role">Technical Support</span>
+                </div>
+            </div>
         </div>
     </div>
+</div>
+<div class="section mt-3">
+    <ul class="listview image-listview">
+        <li>
+            <div class="item">
+                <div class="in">
+                    <div>
+                        <h3>Edit Profile</h3>
+                    </div>
+                        <span class="badge bg-belum">Juni 2024</span>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+<div class="section mt-3">
+    <ul class="listview image-listview">
+        <li>
+            <div class="item">
+                <div class="in">
+                    <div>
+                        <h3>Delete Account</h3>
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+<div class="section mt-3">
+    <ul class="listview image-listview">
+        <li>
+            <div class="item">
+                <div class="in">
+                    <div>
+                        <h3>Mode Gelap</h3>
+                    </div>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+                      </div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+<div class="section mt-3">
+    <ul class="listview image-listview">
+        <li>
+            <div class="item">
+                <div class="in">
+                    <div>
+                        <h3>Log Out</h3>
+                    </div>
+                        <span class="badge bg-belum">Juni 2024</span>
+                </div>
+            </div>
+        </li>
+    </ul>
 </div>
 
 @endsection
