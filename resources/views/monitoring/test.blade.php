d8d24dd5ffbcfa5bf9b9ad6cfb806d901ce66955
Helasdic <95930420+Helasdic@users.noreply.github.com>
2024-06-28 16:40:53 +0700

    oprek timeline

--- resources/views/monitoring/test.blade.php
+++ resources/views/monitoring/test.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.master')
+<link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
 
 @section('header')
     <!-- App Header -->
@@ -83,7 +84,7 @@
                     @foreach($dataBulan[$index + 1] as $data)
                         <ul class="listview image-listview">
                             <li>
-                                <a href="#" id="listCard" class="digi" data-slug="{{ $data->slug }}">
+                                <a href="#" class="listCard digi" data-slug="{{ $data->slug }}">
                                     <div class="item">
                                         <div class="in">
                                             <div>
@@ -93,7 +94,7 @@
                                             </div>
                                             <span class="badge {{ $data->status->status_out_tw == 'Selesai' ? "bg-udah" : "bg-belum" }}">
                                                 @if($data->status->status_out_tw == 'Selesai')
-                                                    selesai
+                                                    Selesai
                                                 @else
                                                     Proses
                                                 @endif
@@ -128,7 +129,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                <button type="button" class="btn btn-secondary" id="closeModalButton" data-bs-dismiss="modal">Tutup</button>
             </div>
         </div>
     </div>
@@ -136,56 +137,231 @@
 
 @push('myscript')
     <script>
-    $(document).ready(function () {
-    var currentSlug = null;
-
-    // Handle card click to load timeline
-    $(document).on('click', '#listCard', function (e) {
-        e.preventDefault();
-        var slug = $(this).data('slug');
-        currentSlug = slug;
-        $('#modal-timeline').modal('show');
-        loadTimeline(slug);
-    });
-
-    // Function to load timeline based on the slug
-    function loadTimeline(slug) {
-        $.ajax({
-            url: '{{ route('get_timeline') }}',
-            type: 'GET',
-            data: { slug: slug },
-            success: function (response) {
-                var timelineHTML = '';
-
-                if (response && response.length > 0) {
-                    $.each(response, function (index, item) {
-                        var stepNumber = index + 1;
-                        var fileAttachment = item.file_name ? '<a href="' + item.file_path + '" class="badge bg-primary text-white" target="_blank">Download Lampiran</a>' : '';
-
-                        timelineHTML += '<div class="card mb-3">';
-                        timelineHTML += '  <div class="card-header"><b>Langkah ' + stepNumber + ':</b> ' + item.judul + '</div>';
-                        timelineHTML += '  <div class="card-body">';
-                        timelineHTML += '    <p class="card-text">' + item.deskripsi + '</p>';
-                        timelineHTML += '    <small class="text-muted">Tanggal: ' + item.tanggal + '</small>';
-                        timelineHTML += '    <div>' + fileAttachment + '</div>';
-                        timelineHTML += '  </div>';
-                        timelineHTML += '</div>';
+                $("#bdaymonth").change(function () {
+                var blth = $("#bdaymonth").val();
+                get_lpp_bulanan(blth);
                     });
-                } else {
-                    timelineHTML = '<p class="text-center">Tidak ada data timeline untuk layanan ini.</p>';
-                }
 
-                $('#judulTimeline').text('Timeline untuk ' + slug);
-                $('#loadTimeline').html(timelineHTML);
-            },
-            error: function (xhr, status, error) {
-                console.error(error);
-                $('#loadTimeline').html('<p class="text-center text-danger">Gagal memuat data timeline.</p>');
+            function get_lpp_bulanan(blth) {
+                var sblth = blth.split("-");
+                var lbulan = [
+                    'Januari',
+                    'Februari',
+                    'Maret',
+                    'April',
+                    'Mei',
+                    'Juni',
+                    'Juli',
+                    'Agustus',
+                    'September',
+                    'Oktober',
+                    'November',
+                    'Desember'
+                ];
+                $.ajax({
+                    type: 'POST',
+                    url: '/get-bapp-bulanan',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    data: {
+                        bulan: sblth[1],
+                        tahun: sblth[0],
+                        _token: _token
+                    },
+                    success: (data) => {
+                        if (data.pesan === 'SUCCESS') {
+                            $('#titleheaderBulanan').html("Monitoring Laporan Pelaksanaan Pekerjaan (BAPP) " + lbulan[parseInt(sblth[1]) - 1] + "-" + sblth[0]);
+                            var t = $('#dashboardLppBulanan').DataTable({
+                                dom: 'Blfrtip',
+                                buttons: [{
+                                    extend: 'excelHtml5',
+                                    extension: '.xlsx',
+                                    className: 'btn-sm btn-primary mb-2',
+                                    text: 'Download Excel',
+                                    title: 'Monitoring Laporan Pelaksanaan Pekerjaan (BAPP) ' + lbulan[data[1]]
+                                }],
+                                destroy: true,
+                                scrollX: true,
+                                data: data.data,
+                                columns: [{
+                                        "data": null,
+                                        "orderable": false,
+                                        "searchable": false,
+                                        "render": function (data, type, row, meta) {
+                                            return meta.row + meta.settings._iDisplayStart + 1;
+                                        }
+                                    },
+                                    {
+                                        data: 'name',
+                                        render: function (data, type, row) {
+                                            return "<p style='font-size:12px;'>" + row.name + "</p>";
+                                        }
+                                    },
+                                    {
+                                        data: 'data.step2',
+                                        render: function (data, type, row) {
+                                            if (row.data.step2 === 'cheked') {
+                                                var render = "<div class='font-30 text-primary text-center'><i class ='fadeIn animated bx bx-check'></i><p class='text-center' style='font-size:12px;'>" + row.data.tanggal2 + "</p></div>";
+                                            } else {
+                                                var render = "";
+                                            }
+                                            return render;
+                                        }
+                                    },
+                                    {
+                                        data: 'data.step3',
+                                        render: function (data, type, row) {
+                                            if (row.data.step3 === 'cheked') {
+                                                var render = "<div class='font-30 text-primary text-center'><i class ='fadeIn animated bx bx-check'></i><p class='text-center' style='font-size:12px;'>" + row.data.tanggal3 + "</p></div>";
+                                            } else {
+                                                var render = "";
+                                            }
+                                            return render;
+                                        }
+                                    },
+                                    {
+                                        data: 'data.step4',
+                                        render: function (data, type, row) {
+                                            if (row.data.step4 === 'cheked') {
+                                                var render = "<div class='font-30 text-primary text-center'><i class ='fadeIn animated bx bx-check'></i><p class='text-center' style='font-size:12px;'>" + row.data.tanggal4 + "</p></div>";
+                                            } else {
+                                                var render = "";
+                                            }
+                                            return render;
+                                        }
+                                    },
+                                    {
+                                        data: 'data.step5',
+                                        render: function (data, type, row) {
+                                            if (row.data.step5 === 'cheked') {
+                                                var render = "<div class='font-30 text-primary text-center'><i class ='fadeIn animated bx bx-check'></i><p class='text-center' style='font-size:12px;'>" + row.data.tanggal5 + "</p></div>";
+                                            } else {
+                                                var render = "";
+                                            }
+                                            return render;
+                                        }
+                                    },
+                                    {
+                                        data: 'data.step6',
+                                        render: function (data, type, row) {
+                                            if (row.data.step6 === 'cheked') {
+                                                var render = "<div class='font-30 text-primary text-center'><i class ='fadeIn animated bx bx-check'></i><p class='text-center' style='font-size:12px;'>" + row.data.tanggal6 + "</p></div>";
+                                            } else {
+                                                var render = "";
+                                            }
+                                            return render;
+                                        }
+                                    },
+                                    {
+                                        data: 'data.step7',
+                                        render: function (data, type, row) {
+                                            if (row.data.step7 === 'cheked') {
+                                                var render = "<div class='font-30 text-primary text-center'><i class ='fadeIn animated bx bx-check'></i><p class='text-center' style='font-size:12px;'>" + row.data.tanggal7 + "</p></div>";
+                                            } else {
+                                                var render = "";
+                                            }
+                                            return render;
+                                        }
+                                    },
+                                    {
+                                        data: 'data.step8',
+                                        render: function (data, type, row) {
+                                            var render = "<p class='font-10 " + row.warna + " text-center'>" + data + "</p><p class='text-center' style='font-size:12px;'>" + row.data.tanggal8 + "</p>";
+                                            return render;
+                                        }
+                                    },
+                                ],
+                                "order": [
+                                    [1, 'asc']
+                                ]
+                            });
+                            t.on('order.dt search.dt', function () {
+                                let i = 1;
+
+                                t.cells(null, 0, {
+                                    search: 'applied',
+                                    order: 'applied'
+                                }).every(function (cell) {
+                                    this.data(i++);
+                                });
+                            }).draw();
+                        } else if (data.pesan === 'ERROR') {
+                            swal("Gagal!", "Layanan Aplikasi baru gagal di tambah!", "error");
+                        } else {
+                            swal("Info!", data.pesan, "info")
+                                .then((value) => {
+                                    location.reload();
+                                });
+                        }
+                    },
+                    error: function (data) {
+                        swal("Oppss!", "Terjadi kesalahan, silahkan coba beberapa saat atau hubunggi tim support!", "error");
+                        $('#btnAddLayananDisabled').addClass('d-none');
+                        $('#btnAddLayanan').removeClass('d-none');
+                    }
+                });
             }
+        $(document).on('click', '.listCard', function (e) {
+            $('#closeModalButton').on('click', function () {
+                $('#modal-timeline').modal('hide');
+            });
+            e.preventDefault();
+            var slug = $(this).data('slug');
+            $.ajax({
+                type: 'POST',
+                url: '{{ route('get_timeline') }}',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                data: {
+                    slug: slug
+                },
+                success: function (data) {
+                    if (data.pesan === 'SUCCESS') {
+                        var html = '';
+                        $('#judulTimeline').text("BAPP " + data.data.layanan.aplikasi.nama_layanan + " Periode " + data.data.layanan.bulan + " - " + data.data.layanan.tahun);
+                        $.each(data.data.timeline, function (index, value) {
+                            var act = index === 0 ? "event2" : "";
+                            var d1 = new Date(value.created_at);
+
+                            function formatDate(date) {
+                                var d = new Date(date),
+                                    month = '' + (d.getMonth() + 1),
+                                    day = '' + d.getDate(),
+                                    year = d.getFullYear();
+
+                                if (month.length < 2)
+                                    month = '0' + month;
+                                if (day.length < 2)
+                                    day = '0' + day;
+
+                                return [day, month, year].join('-');
+                            }
+
+                            var link = value.status.status_tw === 'Selesai'
+                                ? "<a href='/proxy.php?path=" + encodeURIComponent(value.file_path) + "' target='_blank' rel='noopener noreferrer' class='text-decoration-none' data-bs-toggle='tooltip' data-bs-placement='left' title='Klik untuk mengunduh file usulan lampiran'><i class='bx bx-file-blank mr-1'></i> File Lampiran</a>"
+                                : "";
+
+                            var result2 = formatDate(d1);
+
+                            html += "<li class='event " + act + "' data-date='" + result2 + "'>" +
+                                "<h3>" + value.status.status_tw + "</h3>" +
+                                "<p>" + value.keterangan + " " + link + ".</p>" +
+                                "</li>";
+                        });
+                        $('#loadTimeline').html(html);
+                        $('#modal-timeline').modal('show');
+                    } else {
+                        alert("Gagal memuat data timeline.");
+                    }
+                },
+                error: function (xhr, status, error) {
+                    var errorMessage = xhr.status + ': ' + xhr.statusText + '\n' + xhr.responseText;
+                    console.log(errorMessage);
+                    alert('Error - ' + errorMessage);
+                }
+            });
         });
-    }
-});
-
-
-    </script>
+    </script>   
 @endpush
